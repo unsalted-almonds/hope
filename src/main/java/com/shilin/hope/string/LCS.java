75b855b01bc91aaf46542cb8104cc93b60e898ed
shilin-gan <shilin_gan@gallup.com>
2017-03-06 16:56:18 -0600

    update

--- src/main/java/com/shilin/hope/string/LCS.java
+++ src/main/java/com/shilin/hope/string/LCS.java
@@ -22,13 +22,34 @@ public class LCS {
 	 * @return: the length of the longest common substring.
 	 */
 	public int longestCommonSubstring(String A, String B) {
-		// write your code here
-		if (A == null || B == null){
-			return 0;
-		}
-		
-		
-		
-		return 0;
-	}
+        // write your code here
+        if (A == null || A.length() == 0 || B == null || B.length() == 0){
+            return 0;
+        }
+        
+        // use length() + 1 so that lcs[0][0] can be used as dummy starting point later
+        int[][] lcs = new int[A.length() + 1][B.length() + 1];
+        
+        // start from 1 since lcs[0][0] is dummy starting point 
+        for (int i = 1; i <= A.length(); i++){
+            for (int j = 1; j <= B.length(); j++){
+                if (A.charAt(i - 1) == B.charAt(j - 1)){
+                    lcs[i][j] = lcs[i-1][j-1] + 1;
+                }else{
+                    lcs[i][j] = 0;
+                }
+            }
+        }
+        
+        int max = 0;
+        
+        for (int i = 1; i <= A.length(); i++){
+            for (int j = 1; j <= B.length(); j++){
+                max = Math.max(max, lcs[i][j]);
+            }
+        }
+        
+        return max;
+        
+    }
 }
