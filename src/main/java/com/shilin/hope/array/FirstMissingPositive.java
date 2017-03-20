be9a55c126acb6f07b643e390a7367ebcb4e5943
shilin-gan <shilin_gan@gallup.com>
2017-03-20 16:41:40 -0500

    nothing

---
+++ src/main/java/com/shilin/hope/array/FirstMissingPositive.java
@@ -0,0 +1,21 @@
+package com.shilin.hope.array;
+
+/**
+ * Given an unsorted integer array, find the first missing positive integer.
+ * 
+ * Example Given [1,2,0] return 3, and [3,4,-1,1] return 2.
+ * 
+ * @author Shilin_Gan
+ *
+ */
+public class FirstMissingPositive {
+    /**    
+     * @param A: an array of integers
+     * @return: an integer
+     */
+    public int firstMissingPositive(int[] A) {
+    	
+    	
+    	return -99;
+    }
+}
