337a9f1914de460a7d2ce8567a016159f7a1b322
unsalted-almonds <google@gmail.com>
2017-03-23 23:21:34 -0500

    three sum

--- src/main/java/com/shilin/hope/array/ThreeSumClosest.java
+++ src/main/java/com/shilin/hope/array/ThreeSumClosest.java
@@ -1,5 +1,7 @@
 package com.shilin.hope.array;
 
+import java.util.Arrays;
+
 /**
  * Given an array S of n integers, find three integers in S such that the sum is
  * closest to a given number, target. Return the sum of the three integers.
@@ -15,19 +17,69 @@ package com.shilin.hope.array;
  *
  */
 public class ThreeSumClosest {
-
-	/**
-	 * @param numbers:
-	 *            Give an array numbers of n integer
-	 * @param target
-	 *            : An integer
-	 * @return : return the sum of the three integers, the sum closest target.
-	 */
-	public int threeSumClosest(int[] numbers, int target) {
-		// write your code here
+	
+	public static void main(String args[]){
 		
+		int[] numbers = new int[]{2,7,11,15};
+		int target = 3;
 		
-		return -99;
+		new ThreeSumClosest().threeSumClosest(numbers, target);
 	}
+
+    /**
+     * @param numbers: Give an array numbers of n integer
+     * @param target : An integer
+     * @return : return the sum of the three integers, the sum closest target.
+     */
+    public int threeSumClosest(int[] numbers, int target) {
+        // write your code here
+        
+        // ¶Ô×²ĞÍÖ¸Õë
+        
+        if (numbers == null || numbers.length < 3) {
+            return 0;
+        }
+        
+        // [-3, -1, 0 ,1 ,2, 3]
+        
+        Arrays.sort(numbers);
+        
+        int minDistance = Integer.MAX_VALUE;
+        int minSum = Integer.MAX_VALUE;
+        
+        int j, k;
+        
+        for (int i = 0; i < numbers.length; i++){
+            j = i + 1;
+            k = numbers.length - 1;
+            
+            while (j < k){
+                
+                int distance = target - numbers[i] - numbers[j] - numbers[k];
+                int absoluteDist = Math.abs(distance);
+                
+                System.out.println("absoluteDist = " + absoluteDist);
+                System.out.println("i = " + i + ", j = " + j + ", k = " + k);
+                System.out.println("sum = " + (numbers[i] + numbers[j] + numbers[k]));
+                
+                if (absoluteDist == 0){
+                    return target;
+                } else if (absoluteDist < minDistance){
+                    minDistance = absoluteDist;
+                    minSum = numbers[i] + numbers[j] + numbers[k];
+                } 
+                
+                if (distance > 0){
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+            
+        }
+        
+        return minSum;
+        
+    }
 
 }
