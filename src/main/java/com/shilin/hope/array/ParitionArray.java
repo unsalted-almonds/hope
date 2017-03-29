d2c9326f181aa0ec147d7955f8f920d000194cba
unsalted-almonds <google@gmail.com>
2017-03-28 22:17:36 -0500

    parition array

---
+++ src/main/java/com/shilin/hope/array/ParitionArray.java
@@ -0,0 +1,62 @@
+package com.shilin.hope.array;
+
+/**
+ * Given an array nums of integers and an int k, partition the array (i.e move
+ * the elements in "nums") such that:
+ * 
+ * All elements < k are moved to the left All elements >= k are moved to the
+ * right Return the partitioning index, i.e the first index i nums[i] >= k.
+ * 
+ * Notice
+ * 
+ * You should do really partition in array nums instead of just counting the
+ * numbers of integers smaller than k.
+ * 
+ * If all elements in nums are smaller than k, then return nums.length
+ * 
+ * Example If nums = [3,2,2,1] and k=2, a valid answer is 1.
+ * 
+ * @author Shilin
+ *
+ */
+public class ParitionArray {
+	/**
+	 * @param nums:
+	 *            The integer array you should partition
+	 * @param k:
+	 *            As description return: The index after partition
+	 */
+	public int partitionArray(int[] nums, int k) {
+		// write your code here
+
+		// [3 2 2 1] k = 2
+		// [1 2 2 3] return 1
+
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+
+		int ans = nums.length;
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] < k) {
+				if (i > ans) {
+					swap(nums, i, ans);
+					ans++;
+				}
+			} else {
+				if (ans == nums.length) {
+					ans = i;
+				}
+			}
+		}
+
+		return ans;
+	}
+
+	private void swap(int[] nums, int a, int b) {
+		int tmp = nums[a];
+		nums[a] = nums[b];
+		nums[b] = tmp;
+	}
+}
