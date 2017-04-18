aebe226f3ccf6cd2815e25353513eb94ce47a19e
unsalted-almonds <google@gmail.com>
2017-04-17 21:42:42 -0500

    fghjfgjd

---
+++ src/main/java/com/shilin/hope/bitwise/App.java
@@ -0,0 +1,12 @@
+package com.shilin.hope.bitwise;
+
+public class App {
+	public static void main(String args[]) {
+
+		System.out.println("4 binary = " + Integer.toBinaryString(4));
+
+		System.out.println("5 binary = " + Integer.toBinaryString(5));
+
+
+	}
+}
