559548969fcf6e8f235343002d88ed3ea83c34a9
shilin-gan <shilin_gan@gallup.com>
2017-07-24 17:02:24 -0500

    word ladder unfinished

---
+++ src/main/java/com/shilin/hope/searchrecursion/WordLadder.java
@@ -0,0 +1,110 @@
+package com.shilin.hope.searchrecursion;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given two words (start and end), and a dictionary, find the length of
+ * shortest transformation sequence from start to end, such that:
+ * <p>
+ * Only one letter can be changed at a time Each intermediate word must exist in
+ * the dictionary Notice
+ * <p>
+ * Return 0 if there is no such transformation sequence. All words have the same
+ * length. All words contain only lowercase alphabetic characters.
+ * <p>
+ * Example Given: start = "hit" end = "cog" dict =
+ * ["hot","dot","dog","lot","log"] As one shortest transformation is "hit" ->
+ * "hot" -> "dot" -> "dog" -> "cog", return its length 5.
+ *
+ * @author Shilin_Gan
+ */
+public class WordLadder {
+    /**
+     * @param start, a string
+     * @param end,   a string
+     * @param dict,  a set of string
+     * @return an integer
+     */
+
+    public static void main(String[] args) {
+        WordLadder test = new WordLadder();
+        //System.out.println(test.differByOne("123","134"));
+
+        Set<String> dict = new HashSet<String>();
+
+        dict.addAll(Arrays.asList("a","b","c"));
+        System.out.println(test.ladderLength("a", "c", dict));
+
+
+        //dict.addAll(Arrays.asList("kid", "tag", "pup", "ail", "tun", "woo", "erg", "luz", "brr", "gay", "sip", "kay", "per", "val", "mes", "ohs", "now", "boa", "cet", "pal", "bar", "die", "war", "hay", "eco", "pub", "lob", "rue", "fry", "lit", "rex", "jan", "cot", "bid", "ali", "pay", "col", "gum", "ger", "row", "won", "dan", "rum", "fad", "tut", "sag", "yip", "sui", "ark", "has", "zip", "fez", "own", "ump", "dis", "ads", "max", "jaw", "out", "btu", "ana", "gap", "cry", "led", "abe", "box", "ore", "pig", "fie", "toy", "fat", "cal", "lie", "noh", "sew", "ono", "tam", "flu", "mgm", "ply", "awe", "pry", "tit", "tie", "yet", "too", "tax", "jim", "san", "pan", "map", "ski", "ova", "wed", "non", "wac", "nut", "why", "bye", "lye", "oct", "old", "fin", "feb", "chi", "sap", "owl", "log", "tod", "dot", "bow", "fob", "for", "joe", "ivy", "fan", "age", "fax", "hip", "jib", "mel", "hus", "sob", "ifs", "tab", "ara", "dab", "jag", "jar", "arm", "lot", "tom", "sax", "tex", "yum", "pei", "wen", "wry", "ire", "irk", "far", "mew", "wit", "doe", "gas", "rte", "ian", "pot", "ask", "wag", "hag", "amy", "nag", "ron", "soy", "gin", "don", "tug", "fay", "vic", "boo", "nam", "ave", "buy", "sop", "but", "orb", "fen", "paw", "his", "sub", "bob", "yea", "oft", "inn", "rod", "yam", "pew", "web", "hod", "hun", "gyp", "wei", "wis", "rob", "gad", "pie", "mon", "dog", "bib", "rub", "ere", "dig", "era", "cat", "fox", "bee", "mod", "day", "apr", "vie", "nev", "jam", "pam", "new", "aye", "ani", "and", "ibm", "yap", "can", "pyx", "tar", "kin", "fog", "hum", "pip", "cup", "dye", "lyx", "jog", "nun", "par", "wan", "fey", "bus", "oak", "bad", "ats", "set", "qom", "vat", "eat", "pus", "rev", "axe", "ion", "six", "ila", "lao", "mom", "mas", "pro", "few", "opt", "poe", "art", "ash", "oar", "cap", "lop", "may", "shy", "rid", "bat", "sum", "rim", "fee", "bmw", "sky", "maj", "hue", "thy", "ava", "rap", "den", "fla", "auk", "cox", "ibo", "hey", "saw", "vim", "sec", "ltd", "you", "its", "tat", "dew", "eva", "tog", "ram", "let", "see", "zit", "maw", "nix", "ate", "gig", "rep", "owe", "ind", "hog", "eve", "sam", "zoo", "any", "dow", "cod", "bed", "vet", "ham", "sis", "hex", "via", "fir", "nod", "mao", "aug", "mum", "hoe", "bah", "hal", "keg", "hew", "zed", "tow", "gog", "ass", "dem", "who", "bet", "gos", "son", "ear", "spy", "kit", "boy", "due", "sen", "oaf", "mix", "hep", "fur", "ada", "bin", "nil", "mia", "ewe", "hit", "fix", "sad", "rib", "eye", "hop", "haw", "wax", "mid", "tad", "ken", "wad", "rye", "pap", "bog", "gut", "ito", "woe", "our", "ado", "sin", "mad", "ray", "hon", "roy", "dip", "hen", "iva", "lug", "asp", "hui", "yak", "bay", "poi", "yep", "bun", "try", "lad", "elm", "nat", "wyo", "gym", "dug", "toe", "dee", "wig", "sly", "rip", "geo", "cog", "pas", "zen", "odd", "nan", "lay", "pod", "fit", "hem", "joy", "bum", "rio", "yon", "dec", "leg", "put", "sue", "dim", "pet", "yaw", "nub", "bit", "bur", "sid", "sun", "oil", "red", "doc", "moe", "caw", "eel", "dix", "cub", "end", "gem", "off", "yew", "hug", "pop", "tub", "sgt", "lid", "pun", "ton", "sol", "din", "yup", "jab", "pea", "bug", "gag", "mil", "jig", "hub", "low", "did", "tin", "get", "gte", "sox", "lei", "mig", "fig", "lon", "use", "ban", "flo", "nov", "jut", "bag", "mir", "sty", "lap", "two", "ins", "con", "ant", "net", "tux", "ode", "stu", "mug", "cad", "nap", "gun", "fop", "tot", "sow", "sal", "sic", "ted", "wot", "del", "imp", "cob", "way", "ann", "tan", "mci", "job", "wet", "ism", "err", "him", "all", "pad", "hah", "hie", "aim", "ike", "jed", "ego", "mac", "baa", "min", "com", "ill", "was", "cab", "ago", "ina", "big", "ilk", "gal", "tap", "duh", "ola", "ran", "lab", "top", "gob", "hot", "ora", "tia", "kip", "han", "met", "hut", "she", "sac", "fed", "goo", "tee", "ell", "not", "act", "gil", "rut", "ala", "ape", "rig", "cid", "god", "duo", "lin", "aid", "gel", "awl", "lag", "elf", "liz", "ref", "aha", "fib", "oho", "tho", "her", "nor", "ace", "adz", "fun", "ned", "coo", "win", "tao", "coy", "van", "man", "pit", "guy", "foe", "hid", "mai", "sup", "jay", "hob", "mow", "jot", "are", "pol", "arc", "lax", "aft", "alb", "len", "air", "pug", "pox", "vow", "got", "meg", "zoe", "amp", "ale", "bud", "gee", "pin", "dun", "pat", "ten", "mob"));
+        //System.out.println(test.ladderLength("cet", "ism", dict));
+    }
+
+    int result = Integer.MAX_VALUE;
+
+    // "a", "c", ["a","b","c"]
+    // "hit", "cog", ["hot","dot","dog","lot","log"]
+    public int ladderLength(String start, String end, Set<String> dict) {
+        // write your code here
+
+        if (dict == null || dict.size() == 0 || start.length() != end.length()) {
+            return 0;
+        }
+
+        helper(start, end, dict, 1, new HashSet<String>());
+
+        if (result == Integer.MAX_VALUE) {
+            return 0;
+        }
+
+        return result;
+    }
+
+    private void helper(String start, String end, Set<String> dict, int length, Set<String> visited) {
+
+        if (differByOne(start, end)) {
+            if (length + 1 < result) {
+                result = length + 1;
+            }
+            return;
+        }
+
+        visited.add(start);
+        for (String item : dict) {
+            if (length + 1 >= result) {
+                break;
+            }
+
+            if (!visited.contains(item) && differByOne(start, item)) {
+                visited.add(item);
+                helper(item, end, dict, ++length, visited);
+                visited.remove(item);
+                length--;
+            }
+        }
+    }
+
+    // check if two same length strings differ only by one character
+    private boolean differByOne(String a, String b) {
+
+        char[] aArray = a.toCharArray();
+        char[] bArray = b.toCharArray();
+
+        int diff = 0;
+        for (int i = 0; i < aArray.length; i++) {
+            if (aArray[i] != bArray[i]) {
+                if (++diff > 1) {
+                    return false;
+                }
+            }
+        }
+
+        if (diff == 1) {
+            return true;
+        }
+
+        return false;
+    }
+}
