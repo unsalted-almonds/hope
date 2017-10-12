f53ab9b2533fb9de1799fdcad11c41447d57e114
ganshilin <push2gary@gmail.com>
2017-10-11 22:52:30 -0500

    dssssxxxdddddd

---
+++ src/main/java/com/shilin/hope/followup/UserInput.java
@@ -0,0 +1,17 @@
+package com.shilin.hope.followup;
+
+import java.util.Scanner;
+
+public class UserInput {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String firstInput = in.next();
+            String secondInput = in.next();
+
+            System.out.println("first input is " + firstInput + ", second input is " + secondInput);
+        }
+
+    }
+}
