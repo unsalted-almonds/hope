528b8a4996002eb8bc5ea4c5c856140f18328336
ganshilin <push2gary@gmail.com>
2017-10-11 21:19:40 -0500

    dssssxxxdddddd

--- src/main/java/com/shilin/hope/palindrome/LongestPalindrome.java
+++ src/main/java/com/shilin/hope/palindrome/LongestPalindrome.java
@@ -3,6 +3,21 @@ package com.shilin.hope.palindrome;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+ * <p>
+ * This is case sensitive, for example "Aa" is not considered a palindrome here.
+ * <p>
+ * Notice
+ * <p>
+ * Assume the length of given string will not exceed 1010.
+ * <p>
+ * Have you met this question in a real interview? Yes
+ * Example
+ * Given s = "abccccdd" return 7
+ * <p>
+ * One longest palindrome that can be built is "dccaccd", whose length is 7.
+ */
 public class LongestPalindrome {
     /*
      * @param s: a string which consists of lowercase or uppercase letters
