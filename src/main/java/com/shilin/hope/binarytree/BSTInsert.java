fd8debda8bcf444aba71e9becd5bf30a914500f2
ganshilin <push2gary@gmail.com>
2017-10-31 21:09:08 -0500

    reversfdffdfdfdffdssss

--- src/main/java/com/shilin/hope/binarytree/BSTInsert.java
+++ src/main/java/com/shilin/hope/binarytree/BSTInsert.java
@@ -2,81 +2,76 @@ package com.shilin.hope.binarytree;
 
 
 /**
- * Given a binary search tree and a new tree node, insert the node into the
- * tree. You should keep the tree still be a valid binary search tree.
- * 
+ * Given a binary search tree and a new tree node, insert the node into the tree. You should keep the tree still be a
+ * valid binary search tree.
+ * <p>
  * Notice
- * 
+ * <p>
  * You can assume there is no duplicate values in this tree + node.
- * 
- * @author Shilin
  *
+ * @author Shilin
  */
 public class BSTInsert {
-	/**
-	 * @param root:
-	 *            The root of the binary search tree.
-	 * @param node:
-	 *            insert this node into the binary search tree
-	 * @return: The root of the new binary search tree.
-	 */
-	public TreeNode insertNode(TreeNode root, TreeNode node) {
-		// write your code here
+    /**
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        // write your code here
+        if (root == null) {
+            return node;
+        }
 
-		if (root == null) {
-			return node;
-		}
+        TreeNode parent = new TreeNode(Integer.MIN_VALUE);
+        parent.right = root;
+        TreeNode current = root;
 
-		if (node == null) {
-			return root;
-		}
+        while (current != null) {
+            parent = current;
+            // branch left
+            if (node.val < current.val) {
+                current = current.left;
+            }
+            // branch right
+            else {
+                current = current.right;
+            }
+        }
+        if (parent.val > node.val) {
+            parent.left = node;
+        } else {
+            parent.right = node;
+        }
+        return root;
+    }
 
-		TreeNode visited = root;
-		while (true) {
-			if (node.val < visited.val) {
-				if (visited.left == null) {
-					visited.left = node;
-					break;
-				}
-				visited = visited.left;
-			} else {
-				if (visited.right == null) {
-					visited.right = node;
-					break;
-				}
-				visited = visited.right;
-			}
-		}
+    private TreeNode insertNodeRecursive(TreeNode root, TreeNode node) {
+        if (node == null) {
+            return root;
+        }
 
-		return root;
-	}
+        if (root == null) {
+            return node;
+        }
 
-	private TreeNode insertNodeRecursive(TreeNode root, TreeNode node) {
-		if (node == null) {
-			return root;
-		}
+        if (node.val < root.val) {
+            root.left = insertNodeRecursive(root.left, node);
+        } else {
+            root.right = insertNodeRecursive(root.right, node);
+        }
 
-		if (root == null) {
-			return node;
-		}
+        return root;
 
-		if (node.val < root.val) {
-			root.left = insertNodeRecursive(root.left, node);
-		} else {
-			root.right = insertNodeRecursive(root.right, node);
-		}
+    }
 
-		return root;
+    public class TreeNode {
+        public int val;
+        public TreeNode left, right;
 
-	}
-
-	public class TreeNode {
-		public int val;
-		public TreeNode left, right;
-
-		public TreeNode(int val) {
-			this.val = val;
-			this.left = this.right = null;
-		}
-	}
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
 }
