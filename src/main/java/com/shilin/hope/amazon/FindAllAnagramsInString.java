0e52f8062a9382d37661cb9fb5d0d01a7be12354
ganshilin <push2gary@gmail.com>
2017-11-09 22:44:51 -0600

    dddxxxxxxdddxffffxxxd

--- src/main/java/com/shilin/hope/amazon/FindAllAnagramsInString.java
+++ src/main/java/com/shilin/hope/amazon/FindAllAnagramsInString.java
@@ -1,16 +1,24 @@
 package com.shilin.hope.amazon;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
-
- Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 40,000.
-
- The order of output does not matter.
+ * <p>
+ * Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than
+ * 40,000.
+ * <p>
+ * The order of output does not matter.
  */
 public class FindAllAnagramsInString {
+
+    public static void main(String[] args) {
+
+        System.out.println(new FindAllAnagramsInString().findAnagrams("abcdefabcdef", "edc"));
+
+    }
 
     /**
      * @param s a string
@@ -24,9 +32,33 @@ public class FindAllAnagramsInString {
             return result;
         }
 
-        
+        int startIdx = 0;
+        int endIdx = p.length();
 
-        return null;
+        while (endIdx <= s.length()) {
 
+            String tmp = s.substring(startIdx, endIdx);
+            if (isAnagrams(tmp, p)) {
+                result.add(startIdx);
+            }
+            startIdx++;
+            endIdx++;
+        }
+
+        return result;
+
+    }
+
+    private boolean isAnagrams(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        char[] s1Array = s1.toCharArray();
+        char[] s2Array = s2.toCharArray();
+        Arrays.sort(s1Array);
+        Arrays.sort(s2Array);
+
+        return Arrays.equals(s1Array, s2Array);
     }
 }
