7c70c72369651e7eb5e43e283bf0404b336d3d93
shilin-gan <shilin_gan@gallup.com>
2017-11-28 10:19:38 -0600

    ffffffffssxsdsdffffffddd

---
+++ src/main/java/com/shilin/hope/yama/LRUCache.java
@@ -0,0 +1,30 @@
+package com.shilin.hope.yama;
+
+/**
+ * Created by Shilin_Gan on 11/28/2017.
+ */
+public class LRUCache {
+    /*
+    * @param capacity: An integer
+    */public LRUCache(int capacity) {
+        // do intialization if necessary
+    }
+
+    /*
+     * @param key: An integer
+     * @return: An integer
+     */
+    public int get(int key) {
+        // write your code here
+        return -1;
+    }
+
+    /*
+     * @param key: An integer
+     * @param value: An integer
+     * @return: nothing
+     */
+    public void set(int key, int value) {
+        // write your code here
+    }
+}
