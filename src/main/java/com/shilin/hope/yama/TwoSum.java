db3e30826a79f5ef7937b3299c6917e0c8f49250
shilin-gan <shilin_gan@gallup.com>
2017-11-30 10:30:01 -0600

    111d

---
+++ src/main/java/com/shilin/hope/yama/TwoSum.java
@@ -0,0 +1,19 @@
+package com.shilin.hope.yama;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * Created by Shilin_Gan on 11/29/2017.
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+}
