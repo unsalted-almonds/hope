c60dc061a98af41c42a610a37a04280cd898a475
ganshilin <push2gary@gmail.com>
2017-12-03 11:09:29 -0600

    dddddd

--- src/main/java/com/shilin/hope/yama/BSTToGreaterTree.java
+++ src/main/java/com/shilin/hope/yama/BSTToGreaterTree.java
@@ -1,5 +1,7 @@
 package com.shilin.hope.yama;
 
+import java.util.Stack;
+
 /**
  * Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
 
@@ -17,8 +19,46 @@ package com.shilin.hope.yama;
  */
 public class BSTToGreaterTree {
 
+    public static void main(String[] args) {
+        BSTToGreaterTree test = new BSTToGreaterTree();
+
+        TreeNode root = new TreeNode(5);
+        TreeNode left = new TreeNode(2);
+        TreeNode right = new TreeNode(13);
+        root.left = left;
+        root.right = right;
+
+        test.convertBST(root);
+
+        System.out.println("root = " + root.val);
+        System.out.println("left = " + left.val);
+        System.out.println("right = " + right.val);
+    }
+
         public TreeNode convertBST(TreeNode root) {
 
-            return  null;
+            inorder(root);
+
+            return  root;
+        }
+
+        private void inorder(TreeNode root) {
+
+            Stack<TreeNode> stack = new Stack<>();
+
+            TreeNode current = root;
+            int prev = 0;
+            while (!stack.isEmpty() || current != null) {
+
+                while (current != null) {
+                    stack.push(current);
+                    current = current.right;
+                }
+                TreeNode tmp = stack.pop();
+                tmp.val += prev;
+                prev = tmp.val;
+                current = tmp.left;
+            }
+
         }
 }
--- src/main/java/com/shilin/hope/yama/BoundaryOfBinaryTree.java
+++ src/main/java/com/shilin/hope/yama/BoundaryOfBinaryTree.java
@@ -1,5 +1,9 @@
 package com.shilin.hope.yama;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Given a binary tree, return the values of its boundary in anti-clockwise direction starting from root. Boundary includes left boundary, leaves, and right boundary in order without duplicate nodes.
 
@@ -49,5 +53,205 @@ package com.shilin.hope.yama;
  So order them in anti-clockwise without duplicate nodes we have [1,2,4,7,8,9,10,6,3].
  */
 public class BoundaryOfBinaryTree {
-    
+
+    public static void main(String[] args) {
+
+        BoundaryOfBinaryTree test = new BoundaryOfBinaryTree();
+
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node8 = new TreeNode(8);
+        TreeNode node9 = new TreeNode(9);
+        TreeNode node10 = new TreeNode(10);
+
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node4.left = node7;
+        node4.right = node8;
+        node3.left = node6;
+        node6.left = node9;
+        node6.right = node10;
+
+        System.out.println(test.getBoundary(root));
+
+        test.printBoundary(root);
+    }
+
+    private List<Integer> getBoundary(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        res.add(root.val);
+
+        List<Integer> left = getLeft(root);
+        List<Integer> right = getRight(root);
+        List<Integer> leaves = getLeaves(root);
+
+        res.addAll(left);
+
+        if (left.size() > 0) {
+            leaves.remove(0);
+        }
+
+        if (right.size() > 0) {
+            leaves.remove(leaves.size() - 1);
+        }
+        res.addAll(leaves);
+
+        for (int i = right.size() - 1; i >= 0; i--) {
+            res.add(right.get(i));
+        }
+
+        return res;
+    }
+
+    private List<Integer> getLeaves(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode current = root;
+
+        while (!stack.isEmpty() || current != null) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            TreeNode tmp = stack.pop();
+            if (tmp.left == null && tmp.right == null) {
+                res.add(tmp.val);
+            }
+            current = tmp.right;
+        }
+
+        return res;
+    }
+
+    // left boundary without root
+    private List<Integer> getLeft(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        root = root.left;
+        while (root != null) {
+            res.add(root.val);
+            if (root.left != null) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+
+        return res;
+    }
+
+    // right boundary without root
+    private List<Integer> getRight(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        root = root.right;
+
+        while (root != null) {
+            res.add(root.val);
+            if (root.right != null) {
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+
+        return res;
+    }
+
+
+
+    TreeNode root;
+
+    // A simple function to print leaf nodes of a binary tree
+    void printLeaves(TreeNode node)
+    {
+        if (node != null)
+        {
+            printLeaves(node.left);
+
+            // Print it if it is a leaf node
+            if (node.left == null && node.right == null)
+                System.out.print(node.val + " ");
+            printLeaves(node.right);
+        }
+    }
+
+    // A function to print all left boundry nodes, except a leaf node.
+    // Print the nodes in TOP DOWN manner
+    void printBoundaryLeft(TreeNode node)
+    {
+        if (node != null)
+        {
+            if (node.left != null)
+            {
+
+                // to ensure top down order, print the node
+                // before calling itself for left subtree
+                System.out.print(node.val + " ");
+                printBoundaryLeft(node.left);
+            }
+            else if (node.right != null)
+            {
+                System.out.print(node.val + " ");
+                printBoundaryLeft(node.right);
+            }
+
+            // do nothing if it is a leaf node, this way we avoid
+            // duplicates in output
+        }
+    }
+
+    // A function to print all right boundry nodes, except a leaf node
+    // Print the nodes in BOTTOM UP manner
+    void printBoundaryRight(TreeNode node)
+    {
+        if (node != null)
+        {
+            if (node.right != null)
+            {
+                // to ensure bottom up order, first call for right
+                //  subtree, then print this node
+                printBoundaryRight(node.right);
+                System.out.print(node.val + " ");
+            }
+            else if (node.left != null)
+            {
+                printBoundaryRight(node.left);
+                System.out.print(node.val + " ");
+            }
+            // do nothing if it is a leaf node, this way we avoid
+            // duplicates in output
+        }
+    }
+
+    // A function to do boundary traversal of a given binary tree
+    void printBoundary(TreeNode node)
+    {
+        if (node != null)
+        {
+            System.out.print(node.val + " ");
+
+            // Print the left boundary in top-down manner.
+            printBoundaryLeft(node.left);
+
+            // Print all leaf nodes
+            printLeaves(node.left);
+            printLeaves(node.right);
+
+            // Print the right boundary in bottom-up manner
+            printBoundaryRight(node.right);
+        }
+    }
+
 }
