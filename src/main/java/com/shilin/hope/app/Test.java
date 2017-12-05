99fcfe2132fc66cd454d071a36ebdd1f17598324
shilin-gan <shilin_gan@gallup.com>
2017-12-05 17:13:19 -0600

    111ddddd

--- src/main/java/com/shilin/hope/app/Test.java
+++ src/main/java/com/shilin/hope/app/Test.java
@@ -11,40 +11,12 @@ public class Test {
 	
 	public static void main(String args[]){
 		
-		// 11111111111111111111111111111110
-		int num = -2;
-		
-		num = num >>> 1;
-		
-		System.out.println("num >> 1 = " + num + " binary = " + Integer.toBinaryString(num));
+		int i = 1;
+		char p = (char)i;
 
-		
+		System.out.println(p);
 
-	        
-
-	    
 	}
 
-
-public int KthInArrays(int[][] arrays, int k) {
-    // Write your code here
-    
-    List<Integer> allElements = new ArrayList<Integer>();
-    
-//    for (int[] array : arrays){
-//        allElements.addAll(Arrays.asList(array));
-//    }
-    
-    for (int i = 0; i < arrays.length; i++){
-    	
-    	for (int j : arrays[i]){
-    		allElements.add(j);
-    	}
-    	
-    }
-    Collections.reverse(allElements);
-    
-    return allElements.get(k - 1);
-}
 
 }
