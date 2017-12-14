6f3f98a808a4ccdbb7669fe7012e88db58f3f058
shilin-gan <shilin_gan@gallup.com>
2017-12-14 16:29:55 -0600

    111ddddddsfdsfsdfsdfss

---
+++ src/main/java/com/shilin/hope/yama/LowestCommonAncestor.java
@@ -0,0 +1,13 @@
+package com.shilin.hope.yama;
+
+/**
+ * Created by Shilin_Gan on 12/14/2017.
+ */
+public class LowestCommonAncestor {
+
+    public TreeNode solution(TreeNode root, TreeNode p, TreeNode q) {
+
+        return null;
+    }
+
+}
