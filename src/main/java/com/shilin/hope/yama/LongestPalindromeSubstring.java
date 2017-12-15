81b7dbd470e68341df01dc7216c00abe5d9806a7
ganshilin <push2gary@gmail.com>
2017-12-14 23:01:24 -0600

    sadsadsadsad

---
+++ src/main/java/com/shilin/hope/yama/LongestPalindromeSubstring.java
@@ -0,0 +1,53 @@
+package com.shilin.hope.yama;
+
+/**
+ * https://www.youtube.com/watch?v=i9_K0h93PF8
+ */
+public class LongestPalindromeSubstring {
+
+    public static void main(String[] args) {
+        LongestPalindromeSubstring test = new LongestPalindromeSubstring();
+
+        System.out.println(test.longestPalindrome("abcba"));
+    }
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0 || s.length() == 1) {
+            return s;
+        }
+
+        int maxLen = 1;
+        String res = new Character(s.charAt(0)).toString();
+
+        int size = s.length();
+        boolean[][] match = new boolean[size][size];
+
+        // take care of one element and two elements
+        for (int i = 0; i < size; i++) {
+            match[i][i] = true;
+            if (i + 1 < size && s.charAt(i) == s.charAt(i + 1)) {
+                match[i][i + 1] = true;
+                maxLen = 2;
+                res = s.substring(i, i + 2);
+            }
+        }
+
+        // up to bottom is easier
+        for (int i = size - 3; i >= 0; i--) {
+            for (int j = i + 2; j < size; j++) {
+                if (match[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
+                    match[i][j] = true;
+                    int len = j - i + 1;
+                    if (len > maxLen) {
+                        maxLen = len;
+                        res = s.substring(i, j + 1);
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+
+
+}
