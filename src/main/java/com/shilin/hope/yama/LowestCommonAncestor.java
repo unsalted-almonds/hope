4c6d756792349db2d3208dc5ff3b3e2483a0918c
ganshilin <push2gary@gmail.com>
2017-12-14 19:40:39 -0600

    sddsddssdsd

--- src/main/java/com/shilin/hope/yama/LowestCommonAncestor.java
+++ src/main/java/com/shilin/hope/yama/LowestCommonAncestor.java
@@ -1,13 +1,31 @@
 package com.shilin.hope.yama;
 
 /**
- * Created by Shilin_Gan on 12/14/2017.
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+ * <p>
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as
+ * the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+ * <p>
+ * _______3______ /              \ ___5__          ___1__ /      \        /      \ 6      _2       0       8 /  \ 7   4
+ * For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another example is LCA of nodes 5 and 4 is 5,
+ * since a node can be a descendant of itself according to the LCA definition. Created by Shilin_Gan on 12/14/2017.
  */
 public class LowestCommonAncestor {
 
-    public TreeNode solution(TreeNode root, TreeNode p, TreeNode q) {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
 
-        return null;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return (right == null) ? left : right;
+
     }
 
 }
