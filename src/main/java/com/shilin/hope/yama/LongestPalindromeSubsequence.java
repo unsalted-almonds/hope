1b9654597ef4b6c3caf38844346f17a1c2445887
ganshilin <push2gary@gmail.com>
2018-01-05 22:33:03 -0600

    ddddsdsfrgfsdfdgsr

--- src/main/java/com/shilin/hope/yama/LongestPalindromeSubsequence.java
+++ src/main/java/com/shilin/hope/yama/LongestPalindromeSubsequence.java
@@ -28,4 +28,24 @@ public class LongestPalindromeSubsequence {
 
         return dp[0][s.length() - 1];
     }
+
+    /**
+     * public:
+     int longestPalindromeSubseq(string s) {
+     int n = s.size();
+     vector<vector<int>> memo(n, vector<int>(n, -1));
+     return helper(s, 0, n - 1, memo);
+     }
+     int helper(string& s, int i, int j, vector<vector<int>>& memo) {
+     if (memo[i][j] != -1) return memo[i][j];
+     if (i > j) return 0;
+     if (i == j) return 1;
+     if (s[i] == s[j]) {
+     memo[i][j] = helper(s, i + 1, j - 1, memo) + 2;
+     } else {
+     memo[i][j] = max(helper(s, i + 1, j, memo), helper(s, i, j - 1, memo));
+     }
+     return memo[i][j];
+     }
+     */
 }
