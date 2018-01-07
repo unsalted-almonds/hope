f9b5d114ce19f4b69ff7361f05bb94f14537385f
ganshilin <push2gary@gmail.com>
2018-01-07 15:40:25 -0600

    ddddsdsfrgfsdf

--- src/main/java/com/shilin/hope/yama/MyApp.java
+++ src/main/java/com/shilin/hope/yama/MyApp.java
@@ -1,16 +1,12 @@
 package com.shilin.hope.yama;
 
-import java.util.Arrays;
-
 public class MyApp {
 
     public static void main(String args[]) {
 
-        String input = "x+5-3+x=6+x-2" ;
+        String s = "1";
 
-        String[] token = input.split("\\+|-|=");
-
-        System.out.println(Arrays.toString(token));
+        System.out.println(s.substring(1));
 
     }
 }
